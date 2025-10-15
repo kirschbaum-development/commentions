f989a2b380dec8679bf94c2e18b0fe2302012536
zvizvi <5633910@gmail.com>
2025-10-15 14:06:24 +0300

    Add option to hide sidebar in CommentsEntry

--- src/Filament/Infolists/Components/CommentsEntry.php
+++ src/Filament/Infolists/Components/CommentsEntry.php
@@ -6,12 +6,14 @@ use Filament\Infolists\Components\Entry;
 use Kirschbaum\Commentions\Filament\Concerns\HasMentionables;
 use Kirschbaum\Commentions\Filament\Concerns\HasPagination;
 use Kirschbaum\Commentions\Filament\Concerns\HasPolling;
+use Kirschbaum\Commentions\Filament\Concerns\HasSidebar;
 
 class CommentsEntry extends Entry
 {
     use HasMentionables;
     use HasPagination;
     use HasPolling;
+    use HasSidebar;
 
     protected string $view = 'commentions::filament.infolists.components.comments-entry';
 }
