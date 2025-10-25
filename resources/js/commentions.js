eed113d1965180f7f281001f49fca21e8526a878
Luís Dalmolin <luis.nh@gmail.com>
2025-10-25 18:09:08 -0300

    Using default classes to avoid issues

--- resources/js/commentions.js
+++ resources/js/commentions.js
@@ -8,6 +8,8 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('editor', (content, mentions, component, placeholder, editorCssClasses) => {
         let editor
 
+        const defaultEditorCssClasses = `comm:prose comm:dark:prose-invert comm:prose-sm comm:sm:prose-base comm:lg:prose-lg comm:xl:prose-2xl comm:focus:outline-none comm:p-4 comm:min-w-full comm:w-full comm:rounded-lg comm:border comm:border-gray-300 comm:dark:border-gray-700`;
+
         return {
             updatedAt: Date.now(),
 
@@ -30,7 +32,7 @@ document.addEventListener('alpine:init', () => {
                     ],
                     editorProps: {
                         attributes: {
-                            class: editorCssClasses,
+                            class: editorCssClasses || defaultEditorCssClasses,
                         },
                     },
                     placeholder: 'Type something...',
