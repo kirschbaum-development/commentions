54ff862d276249f53fc5e34bac543f2102870130
CSH-Tech <x600555@gmail.com>
2025-11-30 12:55:48 +0200

    Add Hebrew translation

---
+++ resources/lang/he/comments.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'label' => 'תגובות',
+    'placeholder' => 'הקלד את תגובתך...',
+    'no_comments_yet' => 'אין תגובות עדיין.',
+
+    'user_avatar_alt' => 'תמונת משתמש',
+
+    'commented_at' => 'הגיב ב :datetime',
+    'edited_at' => 'נערך ב :datetime',
+    'edited' => 'נערך',
+
+    'delete_comment_heading' => 'מחק תגובה',
+    'delete_comment_body' => 'האם אתה בטוח שברצונך למחוק תגובה זו? לא ניתן לבטל פעולה זו.',
+
+    'cancel' => 'ביטול',
+    'delete' => 'מחק',
+    'save' => 'שמור',
+    'comment' => 'תגובה',
+    'add_reaction' => 'הוסף תגובה',
+    'show_more' => 'הצג עוד',
+
+    'notifications' => 'התראות',
+    'unsubscribe' => 'בטל הרשמה',
+    'subscribe' => 'הירשם',
+    'subscribers' => 'מנויים',
+    'more' => 'עוד|עוד',
+
+    'notification_subscribed' => 'נרשמת לקבלת התראות',
+    'notification_unsubscribed' => 'ההרשמה להתראות בוטלה',
+    'notification_comment_deleted' => 'התגובה נמחקה',
+];
